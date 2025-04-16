f9cd116ece51e22def473a19957763b0d514ab27
jerome-school <924460027@sfsu.edu>
2025-04-15 22:31:20 -0700

    finished it lol

---
+++ README.md
@@ -0,0 +1,24 @@
+# Navbar Express
+A simple Node.js + Express project that serves a webpage with a navigation bar linking to three different routes. 
+
+- Simple frontend navbar
+- Express routing to:
+    - [Climate Crisis Project](https://github.com/jdelacruz23/group-project-1-g10?tab=readme-ov-file)
+    - [Typesetting Project](https://github.com/jdelacruz23/typesetting)
+    - [Four Algorithms Project] (https://github.com/jdelacruz23/four-sorting-algo)
+
+## Tech used
+
+- express
+- node
+- html
+
+## Usage:
+- clone repo
+    - git clone https://github.com/your-username/navbar-express
+- install npm
+- run node server.js
+- open browser and go to http://localhost:3000/<route>
+* /climate-crisis
+* /typesetting
+* /four-algorithms
---
+++ server.js
@@ -0,0 +1,37 @@
+// server.js
+
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// add any necessary code you'd want to!
+
+//root
+app.get('/', (req, res) => {
+    // your nav bar is on index.html
+    res.sendFile(path.join(__dirname, 'public', 'index.html'))
+})
+
+
+// Route for Climate Crisis
+// https://github.com/jdelacruz23/group-project-1-g10?tab=readme-ov-file
+app.get('/climate-crisis', (req, res) => {
+    res.redirect('https://github.com/jdelacruz23/group-project-1-g10?tab=readme-ov-file');
+});
+
+// Route for Typesetting
+// https://github.com/jdelacruz23/typesetting
+app.get('/typesetting', (req, res) => {
+  res.redirect('https://github.com/jdelacruz23/typesetting')
+});
+
+// Route for Four Algorithms
+// https://github.com/jdelacruz23/four-sorting-algo
+app.get('/four-algorithms', (req, res) => {
+  res.redirect('https://github.com/jdelacruz23/four-sorting-algo')
+});
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
